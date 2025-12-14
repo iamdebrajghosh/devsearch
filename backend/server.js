8626a73fd63e644e7106d178fbe738c723b6ecf3
DebrajGhosh <debrajghosh774@gmail.com>
2025-12-14 20:24:59 +0530

    init: repo skeleton (Day 22)

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/api/ping", (req, res) => {
+  res.json({ message: "DevSearch API is running 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
